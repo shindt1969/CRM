e0777a33cb92d1cda76e6465f1bdff059b497d52
shindt1969 <shindt1969@gmail.com>
2022-07-05 17:44:12 +0800

    first commit

---
+++ packages/Webkul/Admin/src/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace Webkul\Admin\Http\Controllers\Admin;
+
+use Carbon\Carbon;
+use Webkul\Admin\Http\Controllers\Controller;
+use Webkul\Admin\Helpers\Dashboard as DashboardHelper;
+
+class DashboardController extends Controller
+{
+    /**
+     * Dashboard object
+     *
+     * @var \Webkul\Admin\Helpers\Dashboard
+     */
+    protected $dashboardHelper;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @param \Webkul\Admin\Helpers\DashboardHelper  $dashboardHelper
+     * @return void
+     */
+    public function __construct(DashboardHelper $dashboardHelper)
+    {
+        $this->dashboardHelper = $dashboardHelper;
+
+        $this->dashboardHelper->setCards();
+        // 建構函式先setcards，
+
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        $cards = $this->dashboardHelper->getCards();
+           // $cards是取得 登入後的dashboard的內容
+        if ($dateRange = request('date-range')) {
+            $dateRange = explode(",", $dateRange);  // explode() 區分數組，利用逗號做區分
+            $endDate = $dateRange[1];               // dateRange的結束時間
+            $startDate = $dateRange[0];             // dateRange的開始時間
+        } else {
+            // Carbon::可以擷取當前時間
+            $endDate = Carbon::now()->format('Y-m-d');
+            $startDate = Carbon::now()->subMonth()->addDays(1)->format('Y-m-d');
+        }
+        return view('admin::dashboard.index', compact('cards', 'startDate', 'endDate'));
+                         //用compact呼叫方式是什麼 cards、startDate、endDate
+                         //利用compact將資料變成   cards=>" "、startDate=>" "、endDate=>" "
+                         //cards的內容
+                         
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function template()
+    {
+        return view('admin::dashboard.template');
+    }
+
+    /**
+     * Returns json data for dashboard card.
+     */
+    public function getCardData()
+    {
+        $cardData = $this->dashboardHelper->getFormattedCardData(request()->all());
+
+        return response()->json($cardData);
+    }
+
+    /**
+     * Returns json data for available dashboard cards.
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function getCards()
+    {
+        $response = $this->dashboardHelper->getCards();;
+
+        $response = array_map(function ($card) {
+            if ($card['view_url'] ?? false) {
+                $card['view_url'] = route($card['view_url'], $card['url_params'] ?? []);
+            }
+
+            return $card;
+        }, $response);
+
+        return response()->json($response);
+    }
+
+    /**
+     * Returns updated json data for available dashboard cards.
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function updateCards()
+    {
+        $requestData = request()->all();
+
+        $cards = $this->dashboardHelper->getCards();
+
+        foreach ($requestData['cards'] as $requestedCardData) {
+            foreach ($cards as $cardIndex => $card) {
+                if (isset($card['card_id'])
+                    && isset($requestedCardData['card_id'])
+                    && $card['card_id'] == $requestedCardData['card_id']
+                ) {
+                    $cards[$cardIndex]['selected'] = $requestedCardData['selected'];
+                }
+            }
+        }
+
+        return response()->json($cards);
+    }
+}
