e55c79d06bc0b0c2376dd16b149ff5f12c983fbe
shindt1969 <shindt1969@gmail.com>
2022-07-15 10:54:31 +0800

    move ResponseJsonController injection into base controller constructor

--- packages/Webkul/Admin/src/Http/Controllers/Controller.php
+++ packages/Webkul/Admin/src/Http/Controllers/Controller.php
@@ -3,12 +3,19 @@
 namespace Webkul\Admin\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use App\Http\Controllers\ResponseJsonController;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
+
+    public function __construct(ResponseJsonController $ResponseJsonController)
+    {
+        $this->ResponseJsonController = $ResponseJsonController;
+    }
+
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     /**
